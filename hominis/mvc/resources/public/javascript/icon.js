9a31d33f480b92bce40163f8665fdec6a16e80ce
Victoria Rios Donadeo <115590637+VictoriaRios@users.noreply.github.com>
2025-02-25 20:52:07 -0300

    Delete hominis directory

--- hominis/mvc/resources/public/javascript/icon.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('scroll', function () {
-    let whatsappIcon = document.getElementById('whatsapp-icon');
-    if (window.scrollY > 100) {
-        whatsappIcon.classList.add('show');
-    } else {
-        whatsappIcon.classList.remove('show');
-    }
-});
